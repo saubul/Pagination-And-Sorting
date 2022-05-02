e85e33910e24f49658ddb4ddebc663e1f8683634
Ruslan <miraxle@yandex.ru>
2022-05-02 17:36:57 +0300

    Инициализация проекта

---
+++ src/main/java/ru/saubulprojects/pagsortapp/PaginationSortingApplication.java
@@ -0,0 +1,13 @@
+package ru.saubulprojects.pagsortapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/saubulprojects/pagsortapp/PaginationSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saubulprojects.pagsortapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

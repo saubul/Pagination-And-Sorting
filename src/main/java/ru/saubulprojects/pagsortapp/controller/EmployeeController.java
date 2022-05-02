31a1786adcd98453ccc545b5617027f2494b8381
Ruslan <miraxle@yandex.ru>
2022-05-02 19:32:55 +0300

    Добавлена страница добавления Employee

--- src/main/java/ru/saubulprojects/pagsortapp/controller/EmployeeController.java
+++ src/main/java/ru/saubulprojects/pagsortapp/controller/EmployeeController.java
@@ -1,20 +1,45 @@
 package ru.saubulprojects.pagsortapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ru.saubulprojects.pagsortapp.model.Employee;
+import ru.saubulprojects.pagsortapp.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
+	private final EmployeeService empService;
+	
+	public EmployeeController(EmployeeService empService) {
+		this.empService = empService;
+	}
+	
 	@GetMapping
-	public String indexForm() {
+	public String indexForm(Model model) {
 		
+		model.addAttribute("employees", empService.getAllEmployees());
 		return "index";
 		
 	}
 	
+	@GetMapping("/new")
+	public String newEmpForm(Model model) {
+		model.addAttribute("emp", new Employee());
+		return "newEmp";
+	}
+	
+	@PostMapping("/new")
+	public String saveEmp(@ModelAttribute("emp") Employee employee) {
+		
+		empService.saveEmployee(employee);
+		return "index";
+	}
 	
 }
 
--- src/main/java/ru/saubulprojects/pagsortapp/service/impl/EmployeeServiceImpl.java
+++ src/main/java/ru/saubulprojects/pagsortapp/service/impl/EmployeeServiceImpl.java
@@ -22,4 +22,9 @@ public class EmployeeServiceImpl implements EmployeeService{
 		return empRepo.findAll();
 	}
 	
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return empRepo.save(employee);
+	}
+	
 }
